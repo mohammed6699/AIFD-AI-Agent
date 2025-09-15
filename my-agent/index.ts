ca201feb84a8584c661a0ab518e4e447d23f0b89
mohammed6699 <mohammedahmed.refaat@gmail.com>
2025-09-15 14:05:55 +0300

    adding the preparing files for the agent

---
+++ my-agent/index.ts
@@ -0,0 +1,9 @@
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
+
+const {text} = await generateText({
+    model: google("models/gemini-2.5-flash"),
+    prompt: "What is an API"
+})
+
+console.log(text)
